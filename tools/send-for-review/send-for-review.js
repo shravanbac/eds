e99a230c0e75ab64a5fbaffcd95d37ca434be62c
shravanbac <shravanaem12@gmail.com>
2025-08-31 14:04:42 -0400

    adding side kick capabilities

--- tools/send-for-review/send-for-review.js
+++ tools/send-for-review/send-for-review.js
@@ -1,4 +1,4 @@
-const DEFAULT_WEBHOOK = 'https://hook.app.workfrontfusion.com/3o5lrlkstfbbrspi35hh0y3cmjkk4gdd';
+const DEFAULT_WEBHOOK = 'https://hook.us2.make.com/6wpuu9mtglv89lsj6acwd8tvbgrfbnko';
 
 /** Resolve webhook URL */
 function resolveWebhook() {
